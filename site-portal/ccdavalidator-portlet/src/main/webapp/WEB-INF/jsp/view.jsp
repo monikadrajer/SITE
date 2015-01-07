8125c830b2dbcc641616381505d581e168c3138b
chris-watson <cxwatson@gmail.com>
2015-01-07 13:40:36 -0500

    Fixed minor issue with Reference C-CDA

--- site-portal/ccdavalidator-portlet/src/main/webapp/WEB-INF/jsp/view.jsp
+++ site-portal/ccdavalidator-portlet/src/main/webapp/WEB-INF/jsp/view.jsp
@@ -468,7 +468,7 @@
 													<label for="CCDAReferenceFileupload">Upload Input File:</label><br/>
 													<span class="btn btn-success fileinput-button" id="CCDAReferenceFileupload-btn"> <i
 															class="glyphicon glyphicon-plus"></i>&nbsp;<span>Upload a File...</span>
-															<input id="CCDAReferenceFileupload" type="file" name="file"  data-parsley-maxsize="3" data-parsley-filetype="xml" data-parsley-required data-parsley-errors-container="#CCDAReferenceInfoArea"  data-parsley-trigger="change" data-parsley-required-message="Please select a C-CDA file." tabindex="1"/>
+															<input id="CCDAReferenceFileupload" type="file" name="file"  data-parsley-maxsize="3" data-parsley-filetype="xml" data-parsley-required data-parsley-errors-container="#CCDAReferenceInfoArea"  data-parsley-trigger="change" data-parsley-required-message="Please select an input file." tabindex="1"/>
 													</span>
 													<div id="CCDAReferenceFiles"></div>
 													<div id="CCDAReferenceInfoArea" class="infoArea"></div>
@@ -488,7 +488,7 @@
 													<label for="CCDAReferenceCEHRTFileupload">Upload C-CDA File:</label><br/>
 													<span class="btn btn-success fileinput-button" id="CCDAReferenceCEHRTFileupload-btn"> <i
 															class="glyphicon glyphicon-plus"></i>&nbsp;<span>Upload a C-CDA File...</span>
-															<input id=CCDAReferenceCEHRTFileupload" type="file" name="ReconciledFile"  data-parsley-maxsize="3" data-parsley-filetype="xml" data-parsley-required data-parsley-errors-container="#CCDACEHRTInfoArea"  data-parsley-trigger="change" data-parsley-required-message="Please select a C-CDA file." tabindex="1"/>
+															<input id="CCDAReferenceCEHRTFileupload" type="file" name="generatedFile"  data-parsley-maxsize="3" data-parsley-filetype="xml" data-parsley-required data-parsley-errors-container="#CCDACEHRTInfoArea"  data-parsley-trigger="change" data-parsley-required-message="Please select a C-CDA file." tabindex="1"/>
 															
 													</span>
 													<div id="CCDACEHRTReferenceFiles"></div>
--- site-portal/ccdavalidator-portlet/src/main/webapp/js/uploader.js
+++ site-portal/ccdavalidator-portlet/src/main/webapp/js/uploader.js
@@ -1378,7 +1378,7 @@ function getDoc(frame) {
 
 
 
-
+//CIRI (Reconciled) C-CDA
 $(function() {
 	
 	var formSelector = "#CCDAReconciledValidationForm";
@@ -1568,6 +1568,212 @@ $(function() {
 	});
 	
 });
+
+
+
+
+// Reference C-CDA Validator
+$(function() {
+	
+	var formSelector = "#CCDAReferenceValidationForm";
+	var ajaximgpath = window.currentContextPath + "/css/ajax-loader.gif";
+	var jform = $(formSelector);
+	
+	
+	// Parsley validator to validate xml extension.
+	window.ParsleyValidator.addValidator('filetype',function(value,requirement){
+		var ext=value.split('.').pop().toLowerCase();
+		return ext === requirement;	
+	},32).addMessage('en','filetype','The selected C-CDA file must be an xml file(.xml)');
+	
+	// parsley Validator to validate CIRI test data file size
+	window.ParsleyValidator.addValidator('referencemaxsize',function(value,requirement){
+		var file_size=$('#CCDAReferenceFileupload')[0].files[0];
+		return file_size.size < requirement*1024*1024;
+	},32).addMessage('en','referencemaxsize','The uploaded file size exceeds the maximum file size of 3 MB.');
+	
+	// parsley Validator to validate the reconciled file size
+	window.ParsleyValidator.addValidator('generatedmaxsize',function(value,requirement){
+		var file_size=$('#CCDAReferenceCEHRTFileupload')[0].files[0];
+		return file_size.size < requirement*1024*1024;
+	},32).addMessage('en','generatedmaxsize','The uploaded file size exceeds the maximum file size of 3 MB.');	
+	
+	
+	// Setting up parsley options to have listed errors and to append the errors to parent element.
+	var parsleyOptions = {
+			errorsWrapper: '<ul></ul>',
+			errorElem: '<li></li>',
+			  errors: {
+			  		classHandler: function(el){
+			 			return el.parent();
+	 					}
+				}
+		};
+	
+	var formInstance = jform.parsley(parsleyOptions);
+	
+	// In the case we have access to the FormData interface:
+	if(window.FormData !== undefined){
+		
+		$('#CCDAReferenceFormSubmit').click(function(e) {
+			
+			e.preventDefault();
+			
+			if(formInstance.validate() === true){
+					
+					var hideMsg = $("#CCDAReferenceFileupload").parsley();
+					window.ParsleyUI.removeError(hideMsg,'required');
+					
+					var hideMsg1 = $("#CCDAReferenceCEHRTFileupload").parsley();
+					window.ParsleyUI.removeError(hideMsg1,'required');
+					
+					
+					$.blockUI({
+						css: {
+					        border: 'none', 
+					        padding: '15px', 
+					        backgroundColor: '#000', 
+					        '-webkit-border-radius': '10px', 
+					        '-moz-border-radius': '10px', 
+					        opacity: .5, 
+					        color: '#fff' 
+				    	},
+				    	message: '<div class="progressorpanel"><img src="'+ ajaximgpath + '" alt="loading">'+
+						          '<div class="lbl">Validating...</div></div>'
+						
+					});
+					
+					var formData = jform.serializefiles();
+					var serviceUrl = jform.attr("action");
+					$.ajax({
+				        url: serviceUrl,
+				        type: 'POST',
+				        
+				        success: function(data){
+				        	
+				        	alert("This has been a call to the Reference validator");
+				        	
+				        	var results = JSON.parse(data);
+				        	$.unblockUI();
+				        	
+				        },
+				        error: errorHandler,
+				        // Form data
+				        data: formData,
+				        //Options to tell JQuery not to process data or worry about content-type
+				        cache: false,
+				        contentType: false,
+				        processData: false
+				    });
+				    
+				}
+			});
+
+		
+	} else { // If no FormData, use a hidden Iframe to submit instead
+		
+		$('#CCDAReferenceFormSubmit').click(function(e) {
+			
+			e.preventDefault();
+			
+			//var formInstance = jform.parsley(parsleyOptions);
+			if(formInstance.validate() === true){
+					
+					var hideMsg = $("#CCDAReferenceFileupload").parsley();
+					window.ParsleyUI.removeError(hideMsg,'required');
+					
+					var hideMsg1 = $("#CCDAReferenceCEHRTFileupload").parsley();
+					window.ParsleyUI.removeError(hideMsg1,'required');
+					
+					
+					$.blockUI({
+						css: {
+					        border: 'none', 
+					        padding: '15px', 
+					        backgroundColor: '#000', 
+					        '-webkit-border-radius': '10px', 
+					        '-moz-border-radius': '10px', 
+					        opacity: .5, 
+					        color: '#fff' 
+				    	},
+				    	message: '<div class="progressorpanel"><img src="'+ ajaximgpath + '" alt="loading">'+
+						          '<div class="lbl">Validating...</div></div>'
+						
+					});
+					
+				    //generate a random id
+				    var  iframeId = 'unique' + (new Date().getTime());
+				    
+				    //create an empty iframe
+				    var iframe = $('<iframe src="javascript:false;" name="'+iframeId+'" id="'+iframeId+'" />');
+				    
+				    //hide it
+				    iframe.hide();
+				    
+				    //set form target to iframe
+				    jform.attr('target',iframeId);
+				    
+				    //Add iframe to body
+				    iframe.appendTo('body');
+				    
+				    iframe.load(function(e)
+				    {
+				        var doc = getDoc(iframe[0]); //get iframe Document
+				        var node = doc.body ? doc.body : doc.documentElement;
+				        var data = (node.innerText || node.textContent);
+				        var results = JSON.parse(data);
+				        
+				        $.unblockUI();
+				        alert("This has been a call to the Reference validator from  an old browser");
+				        
+				    });
+					
+				    jform.submit();
+				    
+				}
+			});
+		
+	}
+	
+	
+	$('#CCDAReferenceFileupload').bind("change", function(){
+		
+		$('#CCDAReferenceFiles').empty();
+		var filePath = $('#CCDAReferenceFileupload').val();
+		fileName = filePath.replace(/^.*[\\\/]/, '');
+		
+		context = $('<div/>').appendTo('#CCDAReferenceFiles');
+		var node = $('<p/>').append($('<span/>').text(fileName));
+		node.appendTo(context);
+	
+	});
+	
+	
+	
+	$('#CCDAReferenceCEHRTFileupload').bind("change", function(){
+		
+		$('#CCDACEHRTReferenceFiles').empty();
+		var filePath = $('#CCDAReferenceCEHRTFileupload').val();
+		fileName = filePath.replace(/^.*[\\\/]/, '');
+		
+		context = $('<div/>').appendTo('#CCDACEHRTReferenceFiles');
+		var node = $('<p/>').append($('<span/>').text(fileName));
+		node.appendTo(context);
+	
+	});
+	
+	
+});
+
+
+
+
+
+
+
+
+
+
 
 
 
